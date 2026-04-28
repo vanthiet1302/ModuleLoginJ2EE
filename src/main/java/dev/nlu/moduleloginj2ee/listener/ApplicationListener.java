0c2511c0d29096eebb114cf20047e9dfb80f8b97
thietvv <thietvv2005@outlook.com>
2026-04-28 19:19:37 +0700

    feat(listener): fix miss annotation

--- src/main/java/dev/nlu/moduleloginj2ee/listener/ApplicationListener.java
+++ src/main/java/dev/nlu/moduleloginj2ee/listener/ApplicationListener.java
@@ -3,10 +3,12 @@ package dev.nlu.moduleloginj2ee.listener;
 import dev.nlu.moduleloginj2ee.util.HibernateUtil;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 
 @Slf4j
+@WebListener
 public class ApplicationListener implements ServletContextListener {
 
     @Override
